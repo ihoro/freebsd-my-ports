1589db3ed57f14a8905b13e54857d26e7324e81c
Igor Ostapenko <pm@igoro.pro>
2023-10-31 20:48:37 +0200

    Upgrade portslab

--- _lab/run.sh
+++ _lab/run.sh
@@ -2,19 +2,25 @@
 
 cd ${0%/*}
 VM_HOME=$PWD
-VM=portslab
+VM_NAME=portslab
+VM_MEM=4G
+VM_MAC=aa:bb:cc:01:02:03
+VM_SSH_PORT=3022
 
-_FREEBSDPORTSLAB_QEMU_MEM=${_FREEBSDPORTSLAB_QEMU_MEM:-4G}
+QEMU_PREFIX=""
+QEMU_PARAMS=""
 
+# TODO: for now the order is very important
+test -f ./d1_usrports.qcow2 && QEMU_PARAMS="${QEMU_PARAMS} -drive file=d1_usrports.qcow2,if=virtio,cache=writethrough "
+
+$QEMU_PREFIX \
 qemu-system-aarch64 \
-  --name $VM \
+  --name $VM_NAME \
   -M virt \
   -accel hvf \
   -cpu host \
   -smp 8,sockets=8,cores=1,threads=1 \
-  -m $_FREEBSDPORTSLAB_QEMU_MEM \
-  -drive file=pflash0.img,format=raw,if=pflash,readonly=on \
-  -drive file=pflash1.img,format=raw,if=pflash \
+  -m $VM_MEM \
   -device virtio-gpu-pci \
   -display default,show-cursor=on \
   -device qemu-xhci \
@@ -22,10 +28,12 @@ qemu-system-aarch64 \
   -device usb-tablet \
   -device intel-hda \
   -device hda-duplex \
-  -drive file=FreeBSD-14.0-CURRENT-arm64-aarch64.raw,format=raw,if=virtio,cache=writethrough \
   -nographic \
   -serial mon:stdio \
-  --nic user,ipv6=off,model=e1000,mac=00:50:4F:52:54:53,hostfwd=tcp:127.0.0.1:3022-:22 \
+  -drive file=/opt/homebrew/opt/qemu/share/qemu/edk2-aarch64-code.fd,if=pflash,format=raw,readonly=on \
+  -drive file=d0_root.qcow2,if=virtio,cache=writethrough \
+  --nic user,ipv6=off,model=e1000,mac=${VM_MAC},hostfwd=tcp:127.0.0.1:${VM_SSH_PORT}-:22 \
+  ${QEMU_PARAMS}
 
 
 
